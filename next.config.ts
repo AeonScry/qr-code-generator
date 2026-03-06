ac7f39d92e94b9aaee7c039e04ad461822a1c401
Jack Teoh <jaxteoh@gmail.com>
2026-03-05 23:14:20 -0600

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const repo = "qr-code-generator"; // <-- change this
 
 const nextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
